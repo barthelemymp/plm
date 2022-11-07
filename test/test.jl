4eaac0504b09d45351138b11e1240e46713c788d
barthelemymp <barthelemy.meynard@polytechnique.edu>
2022-11-07 23:38:26 +0100

    test

--- test/test.jl
+++ test/test.jl
@@ -100,7 +100,7 @@ function main()
     plt = plot(title="corr for lh $(lambdaH) lj $(lambdaJ)")
     plot!(plt,x_list, corr_list, label="Gap Init")
     plot!(plt,x_list, corr_list, label="Data Init")
-    savefig(pfammaping, "../../corr14_lh$(lambdaH)_lj$(lambdaJ).png")
+    savefig(plt, "../../corr14_lh$(lambdaH)_lj$(lambdaJ).png")
 
 
 
