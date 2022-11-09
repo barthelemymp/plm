df2e7da2663e61ddfc3fa5c5aab607c5c5051892
barthelemymp <barthelemy.meynard@polytechnique.edu>
2022-11-09 17:01:55 +0100

    clean

--- data/clean_mutation.py
+++ data/clean_mutation.py
@@ -22,7 +22,7 @@ def write_fasta(dictionary, filename):
             outfile.write(value)
             outfile.write("\n")
 
-def processfasta(fastaPath, file_out, masksavepath):
+def processfasta(fastaPath, file_out, wtPath,wtPath_out, masksavepath):
     seqs = []
     names = []
     for records in SeqIO.parse(fastaPath,"fasta"):
@@ -40,11 +40,25 @@ def processfasta(fastaPath, file_out, masksavepath):
 
     write_fasta(md, file_out)
 
+    mdwt = {}
+    for records in SeqIO.parse(wtPath,"fasta"):
+        names.append(records.id )
+        se = "".join([list(records.seq)[i] for i in range(len(list(records.seq))) if maskMatch[i]])
+        mdwt[records.id ] = se
+
+    write_fasta(mdwt, file_out)
+
+
+
+
 
 for famname in ["AMIE","B3VI55T","BF520","BLAT","BRCA1","BRCA1BRCT","CALM1","DLG4","HG"]: #"AMIE",
     print(famname)
     path_dir = "/Data/barth/mutdata/"
     fastaPath = path_dir + famname+"/"+ "ali"+famname+".fasta"
+    wtPath = path_dir + famname+"/"+ famname+".fasta"
+    wtPath_out = path_dir + famname+"/"+ famname+"_clean.fasta"
     file_out = path_dir + famname+"/"+ "ali"+famname+"_clean.fasta"
     masksavepath = path_dir + famname+"/"+ famname+"mask.npz"
     processfasta(fastaPath, file_out, masksavepath)
+    processfasta(wtPath, wtPath_out, masksavepath)
